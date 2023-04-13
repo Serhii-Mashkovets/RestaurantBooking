7e9fefc12c506a2d6a1fcbc61fe57173148e6d5d
Serhii-Mashkovets <113472286+Serhii-Mashkovets@users.noreply.github.com>
2023-04-13 12:22:20 +0300

    First commit with emphty RestaurantBooking project

---
+++ RestaurantBooking/src/test/java/com/reservation/restaurantBooking/RestaurantBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.restaurantBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
