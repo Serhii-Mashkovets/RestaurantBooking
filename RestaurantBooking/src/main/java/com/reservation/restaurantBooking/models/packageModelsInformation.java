412b29f6604f6870b6564ee8beec7100e78b9f89
Serhii-Mashkovets <113472286+Serhii-Mashkovets@users.noreply.github.com>
2023-04-26 15:09:33 +0300

    added doc-file to explain the need for a model package

---
+++ RestaurantBooking/src/main/java/com/reservation/restaurantBooking/models/packageModelsInformation.java
@@ -0,0 +1,24 @@
+package com.reservation.restaurantBooking.models;
+
+
+/**
+ *
+ * Contains model classes for more convenient presentation and further work.
+ *
+ * All model classes contain private fields with their respective getter and setter methods.
+ * The classes are used to represent different entities in the restaurant booking system such as admins, guests,
+ * reservations, restaurants, and users.
+ * The classes provide easy data access and management for various analytics and reporting purposes.
+ * The Reservation class has additional fields for managing reservation information such as name, date, time,
+ * number of people, and confirmation status.
+ * The CuisineType enum is used to specify the type of cuisine offered by a restaurant,
+ * and the Admin class contains ID and password information for authentication purposes.
+ *
+ * Усі класи моделі містять приватні поля з відповідними методами отримання та встановлення.
+ * Класи використовуються для представлення різних сутностей у системі бронювання ресторанів,
+ * таких як адміністратори, гості, бронювання, ресторани та користувачі.
+ * Класи забезпечують легкий доступ до даних та управління ними для різних аналітичних цілей.
+ * Клас Reservation має додаткові поля для керування інформацією про бронювання, такі як ім'я, дата, час, кількість осіб
+ * та статус підтвердження. Enum CuisineType використовується для визначення типу кухні, яку пропонує ресторан,
+ * а клас Admin містить інформацію про ідентифікатор і пароль для автентифікації.
+ */
